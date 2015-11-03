174eb8668a78946deede32c9de5c4653ab186598
KaoticKibz <Kaotickibz@hotmail.com>
2015-11-03 14:52:19 +0000

    Hola Amigo

---
+++ Brickbreaker/Game1.cs
@@ -0,0 +1,158 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Brickbreaker
+{
+    /// <summary>
+    /// This is the main type for your game.
+    /// </summary>
+    public class Game1 : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;        
+
+        Paddle paddle;
+        Ball ball;
+        Rectangle screenRectangle;
+        int bricksWide = 15;
+        int bricksHigh = 6;
+        Texture2D brickImage;
+        Bricks[,] bricks;
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+
+            graphics.PreferredBackBufferWidth = 1020;
+            graphics.PreferredBackBufferHeight = 700;
+
+            screenRectangle = new Rectangle(
+                0,
+                0,
+                graphics.PreferredBackBufferWidth,
+                graphics.PreferredBackBufferHeight);
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            Texture2D tempTexture = Content.Load<Texture2D>("paddle");
+            paddle = new Paddle(tempTexture, screenRectangle);
+
+            tempTexture = Content.Load<Texture2D>("ball");
+            ball = new Ball(tempTexture, screenRectangle);
+
+            brickImage = Content.Load<Texture2D>("brick");
+
+            StartGame();
+        }
+
+        private void StartGame()
+        {
+            ball.SetInStartPosition(paddle.GetBounds());
+
+            bricks = new Bricks[bricksWide, bricksHigh];
+
+            for (int y = 0; y < bricksHigh; y++)
+            {
+                Color tint = Color.White;
+
+                
+
+                for (int x = 0; x < bricksWide; x++)
+                {
+                    bricks[x, y] = new Bricks(
+                        brickImage,
+                        new Rectangle(
+                            x * brickImage.Width,
+                            y * brickImage.Height,
+                            brickImage.Width,
+                            brickImage.Height),
+                        tint);
+                }
+
+            }
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// game-specific content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            paddle.Update();
+            ball.Update();
+
+            foreach (Bricks brick in bricks)
+            {
+                brick.CheckCollision(ball);
+            }
+
+            ball.PaddleCollision(paddle.GetBounds());
+
+            if (ball.OffBottom())
+                StartGame();
+
+            base.Update(gameTime);
+
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.Black);
+
+            spriteBatch.Begin();
+
+            foreach (Bricks brick in bricks)
+                brick.Draw(spriteBatch);
+
+            paddle.Draw(spriteBatch);
+            ball.Draw(spriteBatch);            
+
+            spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
